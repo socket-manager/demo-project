0770d6b2a35b3c70b3455ebc98c040d5e2d458ee
lib.tech.engineer <lib.tech.engineer@gmail.com>
2024-06-22 23:13:25 +0900

    v1.1.0 Release 1)設定ファイルの機能を追加 →const.phpファイルの追加

---
+++ setting/const.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    /**
+     * @var string ホスト名
+     */
+    'host' => 'localhost',
+
+    /**
+     * @var int ポート番号（UDPマルチサーバーの親ポートと兼用）
+     */
+    'port' => 10000,
+
+    /**
+     * @var int 周期インターバル時間（μs）
+     */
+    'cycle_interval' => 1000,
+
+    /**
+     * @var int アライブチェックタイムアウト時間（μs）
+     */
+    'alive_interval' => 3600,
+
+    /**
+     * @var int 親ポート番号（TCPマルチサーバー用）
+     */
+    'parent_port' => 10010,
+];
